5ebe13b7d0de52fde4dd1f508e8d9f2c3e03f510
Sandeep06-1 <chsandeepshanju06@gmail.com>
2025-01-30 22:44:25 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Declare and initialize a 2D array
+        int[][] array = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        // Access and print elements of the 2D array
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
